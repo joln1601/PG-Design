e91ac81c6732a4538baffbf2bfc9788635f4b861
joln1601 <jonte.97@live.com>
2018-04-05 11:25:30 +0200

    Add project files.

---
+++ pgDesign/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pgDesign.Startup))]
+namespace pgDesign
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
