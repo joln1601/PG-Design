1bcf132c20eca4e6b4c305f9ae2f0d064ac8fa0e
joln1601 <jonte.97@live.com>
2018-04-13 14:45:22 +0200

    Skapat fil för javascript metoder

---
+++ pgDesign/Scripts/JavaScript.js
@@ -0,0 +1,25 @@
+﻿//Script för att skicka rätt bild till modal
+function showimg(id, src) {
+    var modal = document.getElementById('myModal');
+
+    var img = document.getElementById(id)
+    var modalImg = document.getElementById("myImg")
+    modalImg.src = src;
+}
+
+// Script för att sätta bredden på bilderna på startsidan
+var delayInMilliseconds = 250;
+setTimeout(function () {
+    $(document).ready(function () {
+        $("#second_div").css({
+            'width': ($("#first_div").width())
+        });
+        $("#second_div2").css({
+            'width': ($("#first_div2").width())
+        });
+        $("#second_div3").css({
+            'width': ($("#first_div3").width())
+        });
+    });
+}, delayInMilliseconds);
+
