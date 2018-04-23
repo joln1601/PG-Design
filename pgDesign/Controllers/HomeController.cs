9e4575fb7e030c5ecac539ccff5b0d6b0826dcb6
robin_nylander <robin_nylander@hotmail.com>
2018-04-23 10:51:00 +0200

    - DbOperations

--- pgDesign/Controllers/HomeController.cs
+++ pgDesign/Controllers/HomeController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using pgDesign.dbEngine;
+using pgDesign.ViewModels;
 
 namespace pgDesign.Controllers
 {
     public class HomeController : Controller
     {
+        private dbOperation db;
+        private IndexViewModel vm;
+        public HomeController()
+        {
+            db = new dbOperation();
+            vm = new IndexViewModel();
+        }
+        
         public ActionResult Index()
         {
-            return View();
+
+           var Siteinfo = db.AboutText();
+            vm.AboutText = Siteinfo.Content;
+            return View(vm);
         }
 
         public ActionResult About()
--- pgDesign/dbEngine/dbOperation.cs
+++ pgDesign/dbEngine/dbOperation.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using pgDesign.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace pgDesign.dbEngine
 {
     public class dbOperation
     {
+        ApplicationDbContext _DbOperation;
+        public dbOperation()
+        {
+            _DbOperation = new ApplicationDbContext();
+        }
+
+
+        #region Siteinformation
+        public Siteinformation AboutText()
+        {
+            
+            var text = _DbOperation.Siteinformation.Where(c => c.Id == 1).Single();
+            return text;
+        }
+        #endregion
     }
 }
