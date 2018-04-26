7190fb457a0afab84eaec08fba78f60386d3fa3b
joln1601 <jonte.97@live.com>
2018-04-26 15:11:18 +0200

    Gjort om navigationsmenyn

--- pgDesign/Scripts/JavaScript.js
+++ pgDesign/Scripts/JavaScript.js
@@ -28,4 +28,13 @@ setTimeout(function () {
         $(document).ready(function () {
             $('.nav-link li.active').removeClass('active');
         $('a[href="' + location.pathname + '"]').closest('.nav-item').addClass('active');
-    });
+});
+
+//Script för hamburgarikonen (Öppna och stänga sidomenyn änr den är i responsivt läge)
+        function w3_open() {
+            document.getElementById("mySidebar").style.display = "block";
+        }
+        function w3_close() {
+            document.getElementById("mySidebar").style.display = "none";
+        }
+
