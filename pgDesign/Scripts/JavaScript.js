fde9a914ec0f2141544ddd6f7d4c14494480571f
joln1601 <jonte.97@live.com>
2018-04-30 11:33:49 +0200

    Fixat sidomenyn och rensat css och scriptfilen

--- pgDesign/Scripts/JavaScript.js
+++ pgDesign/Scripts/JavaScript.js
@@ -1,14 +1,7 @@
-﻿//Script För att ändra aktiv nav-bar item
-        $(document).ready(function () {
-            $('.nav-link li.active').removeClass('active');
-        $('a[href="' + location.pathname + '"]').closest('.nav-item').addClass('active');
-});
+﻿
 
-//Script för hamburgarikonen (Öppna och stänga sidomenyn änr den är i responsivt läge)
-        function w3_open() {
-            document.getElementById("mySidebar").style.display = "block";
-        }
-        function w3_close() {
-            document.getElementById("mySidebar").style.display = "none";
-        }
 
+
+
+
+       
