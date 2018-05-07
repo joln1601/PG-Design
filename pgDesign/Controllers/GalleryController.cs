884701b9b39707a6adf7d520a1b0e2342d8d7363
robin_nylander <robin_nylander@hotmail.com>
2018-05-07 14:47:15 +0200

    - bildgalleri

--- pgDesign/Controllers/GalleryController.cs
+++ pgDesign/Controllers/GalleryController.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using pgDesign.dbEngine;
+using pgDesign.ViewModels;
+using pgDesign.Models;
 
 namespace pgDesign.Controllers
 {
     public class GalleryController : Controller
     {
+        private AzureBlobHelper AB;
+        private GalleryViewModel gvm;
+        private postedFileModel pfm;
         // GET: Gallery
+        public GalleryController()
+        {
+            AB = new AzureBlobHelper();
+            gvm = new GalleryViewModel();
+            pfm = new postedFileModel();
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Platjobb()
+        {
+            return View();
+        }
+        public ActionResult Lackering()
+        {
+            string ContainerName = "lackering";
+
+            gvm.LackeringBlobList = AB.GetListOfData(pfm, ContainerName);
+            return View(gvm);
         }
     }
 }
---
+++ pgDesign/ViewModels/GalleryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pgDesign.ViewModels
+{
+    public class GalleryViewModel
+    {
+        public List<string> LackeringBlobList { get; set; }
+    }
+}
