d66a342eed327beeb2af8e7e931032dea662929f
joln1601 <jonte.97@live.com>
2018-05-08 14:16:54 +0200

    Ändrat i footer och föröt över js

--- pgDesign/Scripts/JavaScript.js
+++ pgDesign/Scripts/JavaScript.js
@@ -15,6 +15,13 @@ $('.getSrc').click(function () {
     $('.modal-title').attr('name', name);
 });
 
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/sv_SE/sdk.js#xfbml=1&version=v2.12';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 
 
