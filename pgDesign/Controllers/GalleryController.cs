a49c261662d9305dae7acbdc1e0a88d538431211
robin_nylander <robin_nylander@hotmail.com>
2018-05-21 12:43:09 +0200

    - galleri mer dynamiskt

--- pgDesign/Controllers/GalleryController.cs
+++ pgDesign/Controllers/GalleryController.cs
@@ -24,57 +24,20 @@ namespace pgDesign.Controllers
 
         public ActionResult Index(string id)
         {
-            int? idet = Convert.ToInt32(id);
-            if(idet == null || idet == 1)
+          
+            if(id == null)
             {
-            string bildekor = "bildekor";
-            gvm.BlobList = AB.GetListOfData(pfm, bildekor);
+            id = "bildekor";
+            gvm.BlobList = AB.GetListOfData(pfm, id);
                 return View(gvm);
             }
-            else if (idet == 2)
+            else
             {
-                string ombyggnadavfordon = "ombyggnadavfordon";
-                gvm.BlobList = AB.GetListOfData(pfm, ombyggnadavfordon);
-                return View(gvm);
-            }
-            else if (idet == 3)
-            {
-                string utstallningarochinredningar = "utstallningarochinredningar";
-                gvm.BlobList = AB.GetListOfData(pfm, utstallningarochinredningar);
-                return View(gvm);
-            }
-            else if (idet == 4)
-            {
-                string skyltar = "skyltar";
-                gvm.BlobList = AB.GetListOfData(pfm, skyltar);
-                return View(gvm);
-            }
-            else if (idet == 5)
-            {
-                string arbetsmiljolosningar = "arbetsmiljolosningar";
-                gvm.BlobList = AB.GetListOfData(pfm, arbetsmiljolosningar);
-                return View(gvm);
-            }
-            else if (idet == 6)
-            {
-                string offentligutsmyckning = "offentligutsmyckning";
-                gvm.BlobList = AB.GetListOfData(pfm, offentligutsmyckning);
-                return View(gvm);
-            }
-            else if (idet == 7)
-            {
-                string legotillverkningochinnovationer = "legotillverkningochinnovationer";
-                gvm.BlobList = AB.GetListOfData(pfm, legotillverkningochinnovationer);
-                return View(gvm);
-            }
-            else if (idet == 8)
-            {
-                string ovrigt = "ovrigt";
-                gvm.BlobList = AB.GetListOfData(pfm, ovrigt);
-                return View(gvm);
+            gvm.BlobList = AB.GetListOfData(pfm, id);
+            return View(gvm);
             }
 
-            return View();
+      
             
         }
        
