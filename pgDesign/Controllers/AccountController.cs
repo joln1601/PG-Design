988ac0554185e76f50b2ac3cc83d759ad50fdf32
Jonathan Lönnfors <joln1601@student.miun.se>
2019-05-05 14:29:15 +0200

    kommentar

--- pgDesign/Controllers/AccountController.cs
+++ pgDesign/Controllers/AccountController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using pgDesign.Models;
-//kommentar
+//kommentarer
 namespace pgDesign.Controllers
 {
     [Authorize]
